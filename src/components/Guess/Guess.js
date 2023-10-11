dc3fa2ba782518813bbcf8f3fcec1ca60d69b145
fersayago <sayago.fernando@gmail.com>
2023-10-11 19:49:35 -0300

    added guess input

---
+++ src/components/Guess/Guess.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Guess() {
+
+  const [guess, setGuess] = React.useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(guess);
+    setGuess('');
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        onChange={(e)=>{
+          setGuess(e.target.value.toUpperCase());
+        }}
+      />
+    </form>
+  );
+}
+
+export default Guess;
